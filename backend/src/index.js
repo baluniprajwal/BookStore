09dbb18e40967532740b435213e06ab4233bc0e8
prajwal <baluniprajwal@gmail.com>
2025-07-18 01:24:55 +0530

    added book routes and cloudinary

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from "express"
 import "dotenv/config";
 import cors from "cors";
-import { connectDB } from "./database/db.js";
+import { connectDB } from "./utils/db.js";
 import userRoutes from "./routes/userRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js"
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth",userRoutes);
-//app.use("/api/books",bookRoutes);
+app.use("/api/books",bookRoutes);
 
 
 app.listen(3000,()=>{
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,8 +1,14 @@
 import express from "express"
+import protectRoute from "../middlewares/authMiddleware.js";
+import { createBook, deleteBook, getBooks, getRecommendedBooks } from "../controllers/bookController.js";
 
 
 const router = express.Router();
 
+router.post("/",protectRoute,createBook);
+router.get("/",protectRoute,getBooks);
+router.get("/user",protectRoute,getRecommendedBooks);
+router.delete("/:id",protectRoute,deleteBook);
 
 
 
