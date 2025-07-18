754403900e663c382a0cd020dfd619818750defa
prajwal <baluniprajwal@gmail.com>
2025-07-19 02:20:40 +0530

    added cron job

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,15 @@ import cors from "cors";
 import { connectDB } from "./utils/db.js";
 import userRoutes from "./routes/userRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
+import job from "./utils/cron.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
+
 
 app.use("/api/auth",userRoutes);
 app.use("/api/books",bookRoutes);
